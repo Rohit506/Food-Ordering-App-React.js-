325b1781002ea75607518c5d280896c7f582aca0
Rohit <rohit05march@gmail.com>
2021-06-05 16:47:46 +0530

    A complete project Food Order App

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Fragment } from 'react';
+
+import MealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = props => {
+    return(
+        <Fragment>
+            <header className={classes.header} >
+                <h1>ApnaMeals</h1>
+                <HeaderCartButton onClick={props.onShowCart} />
+            </header>
+            <div className={classes['main-image']} >
+                <img src = {MealsImage} alt = "A table full of delecious food!" />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header;
