d2a26458f0da347a1c338d44462eb77f51e9de07
Rohit <rohit05march@gmail.com>
2021-06-16 17:00:08 +0530

    Add showing Loading... utill data is not fetched

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -5,6 +5,7 @@ import Card from "../UI/Card";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     const fetchMeals = async () => {
      const response = await fetch('https://react-food-app-44b28-default-rtdb.firebaseio.com/meals.json');
@@ -22,10 +23,20 @@ const AvailableMeals = () => {
      }
 
      setMeals(loadedMeals);
+     setIsLoading(false);
     };
 
     fetchMeals();
   }, []);
+
+  if(isLoading){
+    return(<>
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    </>)
+  }
+
   const mealsList = meals.map((meal) => (<MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price} /> ));
   return (
     <section classes={classes.meals}>
